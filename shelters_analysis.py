4e3672884329db01c88fecd6e3a1a184926cd7b0
Mariia Kobycheva <mkobycheva@kse.org.ua>
2026-06-29 19:35:22 +0300

    style changes

--- shelters_analysis.py
+++ shelters_analysis.py
@@ -338,7 +338,7 @@ if section == "Місткість":
         locations="district",
         featureidkey="properties.district",
         color="population_by_capacity",
-        color_continuous_scale="Thermal",
+        color_continuous_scale="Purp",
         mapbox_style="carto-positron",
         zoom=9.3,
         center={"lat": 50.40, "lon": 30.52},
