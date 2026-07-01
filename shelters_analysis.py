dfc5db40080b1031a75ee4133aaf4c6eeb422f6f
Mariia Kobycheva <mkobycheva@kse.org.ua>
2026-07-01 17:21:49 +0300

    style changes

--- shelters_analysis.py
+++ shelters_analysis.py
@@ -561,7 +561,7 @@ elif section == "Типи укриттів":
         category_orders={
             "Тип": shelter_kinds_categories
         },
-        color_discrete_sequence=["red", "green", "blue", "goldenrod", "magenta"],
+        color_discrete_sequence=px.colors.qualitative.T20,
         orientation="h",
         barmode="stack",
         labels={"values": "% укриттів" if shelter_kind_percent else "Кількість"},
