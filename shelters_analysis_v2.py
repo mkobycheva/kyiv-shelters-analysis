1faeb25a4bad1ee9d1626d0074c83f4895dd92aa
Mariia Kobycheva <mkobycheva@kse.org.ua>
2026-07-17 03:23:03 +0300

    style changes

--- shelters_analysis_v2.py
+++ shelters_analysis_v2.py
@@ -776,7 +776,7 @@ def render_type_card(t, card_idx):
     photo_idx = st.session_state[state_key] % len(photos)
 
     with st.container(border=True):
-        col_img, col_text = st.columns([1, 1])
+        col_img, col_text = st.columns([1, 2])
         with col_img:
             card_thumbnail(photos[photo_idx], width=260, height=180)
             if len(photos) > 1:
